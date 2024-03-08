64549c78242bea1a9d434c40d2cc92bf26bd2f4e
Augustine Smith <absaugustineflash@gmail.com>
2024-03-08 22:16:51 +0000

    feat: room template

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/The-flash/scalable-websockets-kafka
 
-go 1.21.4
+go 1.22.1
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func roomHandler(w http.ResponseWriter, r *http.Request) {
+	templ := template.Must(template.ParseFiles("room.html"))
+	templ.Execute(w, nil)
+}
 
 func main() {
-	fmt.Println("Scalable Websockets Golang")
+	port := "8080"
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /room/{id}", roomHandler)
+
+	fmt.Printf("Server is running on port %s\n", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), mux); err != nil {
+		fmt.Println(err)
+	}
 }
